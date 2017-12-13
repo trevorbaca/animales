4068d23e330242e357f75e98854e804986ad2270
Trevor Bača <trevor.baca@gmail.com>
2017-12-13 11:34:41 -0600

    Added metronome marks.

---
+++ animales/materials/metronome_marks/definition.py
@@ -0,0 +1,37 @@
+import abjad
+
+
+metronome_marks = abjad.MetronomeMarkDictionary([
+    (
+        '48',
+        abjad.MetronomeMark((1, 4), 48),
+        ),
+    (
+        '60',
+        abjad.MetronomeMark((1, 4), 60),
+        ),
+    (
+        '72',
+        abjad.MetronomeMark((1, 4), 72),
+        ),
+    (
+        '84',
+        abjad.MetronomeMark((1, 4), 84),
+        ),
+    (
+        '96',
+        abjad.MetronomeMark((1, 4), 96),
+        ),
+    (
+        '108',
+        abjad.MetronomeMark((1, 4), 108),
+        ),
+    (
+        '120',
+        abjad.MetronomeMark((1, 4), 120),
+        ),
+    (
+        '132',
+        abjad.MetronomeMark((1, 4), 132),
+        ),
+    ])
