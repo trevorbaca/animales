915684bd107a0588be9364cce722286725e9a857
Trevor Bača <trevor.baca@gmail.com>
2018-01-24 11:30:39 -0600

    Renamed build.

--- animales/builds/ledger-score/__metadata__.py
+++ animales/builds/ledger-score/__metadata__.py
@@ -3,8 +3,8 @@ import abjad
 
 metadata = abjad.TypedOrderedDict(
     [
-        ('catalog_number_suffix', 'ledger'),
-        ('paper_size', 'ledger'),
+        ('catalog_number_suffix', 'ledger-score'),
+        ('paper_size', 'ledger-score'),
         ('price', '\\$156 / \\euro 156'),
         ]
     )
