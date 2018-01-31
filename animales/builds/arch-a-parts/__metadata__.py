b46df47c37835e12f445b2603575b4fa3e8c1136
Trevor Bača <trevor.baca@gmail.com>
2018-01-31 17:07:35 -0600

    Parts.

--- animales/builds/arch-a-parts/__metadata__.py
+++ animales/builds/arch-a-parts/__metadata__.py
@@ -4,7 +4,194 @@ import abjad
 metadata = abjad.OrderedDict(
     [
         (
+            'ARCH_A_PARTS_VN_1_10_VN_1_10',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_11_VN_1_11',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_12_VN_1_12',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_13_VN_1_13',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_14_VN_1_14',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_15_VN_1_15',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_16_VN_1_16',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_17_VN_1_17',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_18_VN_1_18',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
             'ARCH_A_PARTS_VN_1_1_VN_1_1',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_2_VN_1_2',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_3_VN_1_3',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_4_VN_1_4',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_5_VN_1_5',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_6_VN_1_6',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_7_VN_1_7',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_8_VN_1_8',
+            abjad.OrderedDict(
+                [
+                    (
+                        'bol_measure_numbers',
+                        [1, 7, 13, 21, 28],
+                        ),
+                    ]
+                ),
+            ),
+        (
+            'ARCH_A_PARTS_VN_1_9_VN_1_9',
             abjad.OrderedDict(
                 [
                     (
--- animales/builds/arch-a-parts/first_violin_ii_layout.py
+++ animales/builds/arch-a-parts/first_violin_ii_layout.py
@@ -3,8 +3,21 @@ import baca
 
 part_abbreviation = 'VN_1_2'
 
+padding = 6
+start = 50
+separation = 30
 breaks = baca.breaks(
     baca.page( # 1
-        [1, 60, (10,)],
+        [1, start, (padding,)],
+        [7, start + 1 * separation, (padding,)],
+        [13, start + 2 * separation, (padding,)],
+        [21, start + 3 * separation, (padding,)],
+        [28, start + 4 * separation, (padding,)],
         ),
+    )
+
+spacing = baca.scorewide_spacing(
+    __file__,
+    breaks=breaks,
+    fallback_duration=(1, 12),
     )
