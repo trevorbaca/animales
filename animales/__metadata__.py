fab8c54ee37b011282b08e2b5a98d275f64b7e7b
Trevor Bača <trevor.baca@gmail.com>
2018-02-05 11:56:39 -0600

    Parts.

--- animales/__metadata__.py
+++ animales/__metadata__.py
@@ -5,6 +5,192 @@ metadata = abjad.OrderedDict(
     [
         ('catalog_number', 'AWN-018'),
         ('composer', 'Bača'),
+        (
+            'container_to_part',
+            abjad.OrderedDict(
+                [
+                    (
+                        '_1',
+                        abjad.OrderedDict(
+                            [
+                                (
+                                    'iA_FirstViolinVoiceI_A',
+                                    abjad.Part('FirstViolin', (1, 10)),
+                                    ),
+                                (
+                                    'iA_FirstViolinVoiceII_A',
+                                    abjad.Part('FirstViolin', (11, 18)),
+                                    ),
+                                (
+                                    'iA_SecondViolinVoiceI_A',
+                                    abjad.Part('SecondViolin', (1, 10)),
+                                    ),
+                                (
+                                    'iA_SecondViolinVoiceII_A',
+                                    abjad.Part('SecondViolin', (11, 18)),
+                                    ),
+                                (
+                                    'iA_ViolaVoiceI_A',
+                                    abjad.Part('Viola', (1, 10)),
+                                    ),
+                                (
+                                    'iA_ViolaVoiceII_A',
+                                    abjad.Part('Viola', (11, 18)),
+                                    ),
+                                (
+                                    'iA_CelloVoiceI_A',
+                                    abjad.Part('Cello'),
+                                    ),
+                                ]
+                            ),
+                        ),
+                    (
+                        '_2',
+                        abjad.OrderedDict(
+                            [
+                                (
+                                    'iB_FirstViolinVoiceI_A',
+                                    abjad.Part('FirstViolin', (1, 10)),
+                                    ),
+                                (
+                                    'iB_FirstViolinVoiceII_A',
+                                    abjad.Part('FirstViolin', (11, 18)),
+                                    ),
+                                (
+                                    'iB_SecondViolinVoiceI_A',
+                                    abjad.Part('SecondViolin', (1, 10)),
+                                    ),
+                                (
+                                    'iB_SecondViolinVoiceII_A',
+                                    abjad.Part('SecondViolin', (11, 18)),
+                                    ),
+                                (
+                                    'iB_ViolaVoiceI_A',
+                                    abjad.Part('Viola', (1, 10)),
+                                    ),
+                                (
+                                    'iB_ViolaVoiceII_A',
+                                    abjad.Part('Viola', (11, 18)),
+                                    ),
+                                (
+                                    'iB_CelloVoiceI_A',
+                                    abjad.Part('Cello'),
+                                    ),
+                                ]
+                            ),
+                        ),
+                    (
+                        '_3',
+                        abjad.OrderedDict(
+                            [
+                                (
+                                    'iC_FirstViolinVoiceI_A',
+                                    abjad.Part('FirstViolin', (1, 10)),
+                                    ),
+                                (
+                                    'iC_FirstViolinVoiceII_A',
+                                    abjad.Part('FirstViolin', (11, 18)),
+                                    ),
+                                (
+                                    'iC_SecondViolinVoiceI_A',
+                                    abjad.Part('SecondViolin', (1, 10)),
+                                    ),
+                                (
+                                    'iC_SecondViolinVoiceII_A',
+                                    abjad.Part('SecondViolin', (11, 18)),
+                                    ),
+                                (
+                                    'iC_ViolaVoiceI_A',
+                                    abjad.Part('Viola', (1, 10)),
+                                    ),
+                                (
+                                    'iC_ViolaVoiceII_A',
+                                    abjad.Part('Viola', (11, 18)),
+                                    ),
+                                (
+                                    'iC_CelloVoiceI_A',
+                                    abjad.Part('Cello'),
+                                    ),
+                                ]
+                            ),
+                        ),
+                    (
+                        'A1',
+                        abjad.OrderedDict(
+                            [
+                                (
+                                    'AA_FirstViolinVoiceI_A',
+                                    abjad.Part('FirstViolin', (1, 10)),
+                                    ),
+                                (
+                                    'AA_FirstViolinVoiceI_B',
+                                    abjad.Part('FirstViolin'),
+                                    ),
+                                (
+                                    'AA_FirstViolinVoiceII_A',
+                                    abjad.Part('FirstViolin', (11, 18)),
+                                    ),
+                                (
+                                    'AA_SecondViolinVoiceI_A',
+                                    abjad.Part('SecondViolin', (1, 10)),
+                                    ),
+                                (
+                                    'AA_SecondViolinVoiceI_B',
+                                    abjad.Part('SecondViolin'),
+                                    ),
+                                (
+                                    'AA_SecondViolinVoiceII_A',
+                                    abjad.Part('SecondViolin', (11, 18)),
+                                    ),
+                                (
+                                    'AA_ViolaVoiceI_A',
+                                    abjad.Part('Viola', (1, 10)),
+                                    ),
+                                (
+                                    'AA_ViolaVoiceI_B',
+                                    abjad.Part('Viola'),
+                                    ),
+                                (
+                                    'AA_ViolaVoiceII_A',
+                                    abjad.Part('Viola', (11, 18)),
+                                    ),
+                                (
+                                    'AA_CelloVoiceI_A',
+                                    abjad.Part('Cello'),
+                                    ),
+                                (
+                                    'AA_CelloVoiceI_B',
+                                    abjad.Part('Cello'),
+                                    ),
+                                ]
+                            ),
+                        ),
+                    (
+                        'A2',
+                        abjad.OrderedDict(
+                            [
+                                (
+                                    'AB_FirstViolinVoiceI_A',
+                                    abjad.Part('FirstViolin'),
+                                    ),
+                                (
+                                    'AB_SecondViolinVoiceI_A',
+                                    abjad.Part('SecondViolin'),
+                                    ),
+                                (
+                                    'AB_ViolaVoiceI_A',
+                                    abjad.Part('Viola'),
+                                    ),
+                                (
+                                    'AB_CelloVoiceI_A',
+                                    abjad.Part('Cello'),
+                                    ),
+                                ]
+                            ),
+                        ),
+                    ]
+                ),
+            ),
         ('forces_tagline', 'for orchestra'),
         (
             'time_signatures',
