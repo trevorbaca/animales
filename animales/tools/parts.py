d7cae30adacb239e336165a2a53be5a107ff5f3b
Trevor Bača <trevor.baca@gmail.com>
2018-03-01 10:08:51 -0600

    Rerendered segments.

--- animales/tools/parts.py
+++ animales/tools/parts.py
@@ -16,19 +16,19 @@ def parts(
     ..  container:: example
 
         >>> animales.parts('Horn')
-        ContainerCommand(identifier="PartAssignment('Horn')", selector=baca.leaves())
+        PartAssignmentCommand(part_assignment=PartAssignment('Horn'), selector=baca.leaves())
 
         >>> animales.parts('Horn', 1)
-        ContainerCommand(identifier="PartAssignment('Horn', 1)", selector=baca.leaves())
+        PartAssignmentCommand(part_assignment=PartAssignment('Horn', 1), selector=baca.leaves())
 
         >>> animales.parts('Horn', 2)
-        ContainerCommand(identifier="PartAssignment('Horn', 2)", selector=baca.leaves())
+        PartAssignmentCommand(part_assignment=PartAssignment('Horn', 2), selector=baca.leaves())
 
         >>> animales.parts('Horn', (3, 4))
-        ContainerCommand(identifier="PartAssignment('Horn', (3, 4))", selector=baca.leaves())
+        PartAssignmentCommand(part_assignment=PartAssignment('Horn', (3, 4)), selector=baca.leaves())
 
         >>> animales.parts('Horn', [1, 3])
-        ContainerCommand(identifier="PartAssignment('Horn', [1, 3])", selector=baca.leaves())
+        PartAssignmentCommand(part_assignment=PartAssignment('Horn', [1, 3]), selector=baca.leaves())
 
     '''
     part_assignment = abjad.PartAssignment(section=section, members=members)
