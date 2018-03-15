9ca4c4ef06b1b473e6d4b437f209c848cacc0b5b
Trevor Bača <trevor.baca@gmail.com>
2018-03-14 22:08:56 -0500

    Parts.

--- animales/builds/letter-parts/piano/piano_layout.py
+++ animales/builds/letter-parts/piano/piano_layout.py
@@ -3,8 +3,40 @@ import baca
 
 part_abbreviation = 'PF'
 
+def make_systems(measure_numbers, between_systems=20):
+    global_context_padding = 0
+    systems = []
+    if measure_numbers[0] == 1:
+        first_page = True
+    else:
+        first_page = False
+    if first_page:
+        top_margin = 50
+    else:
+        top_margin = 10
+    for i, measure_number in enumerate(measure_numbers):
+        y_offset = top_margin + i * between_systems
+        system = baca.system(
+            global_context_padding,
+            measure=measure_number,
+            y_offset=y_offset,
+            )
+        systems.append(system)
+    return systems
+
 breaks = baca.breaks(
-    baca.page( # 1
-        [1, 60, (10,)],
+    baca.page(
+        *make_systems([1, 11, 21, 31, 41, 51, 61], 24),
+        number=1,
         ),
+    baca.page(
+        *make_systems([76, 88, 99, 111, 121, 131, 141, 151, 161, 171], 22),
+        number=2,
+        ),
+    )
+
+spacing = baca.scorewide_spacing(
+    __file__,
+    breaks=breaks,
+    fallback_duration=(1, 8),
     )
