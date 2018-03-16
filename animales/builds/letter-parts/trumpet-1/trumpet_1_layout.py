e9c3c1657bd7c50be211b11c7db67d80b66d6c23
Trevor Bača <trevor.baca@gmail.com>
2018-03-15 21:41:43 -0500

    Brass parts.

--- animales/builds/letter-parts/trumpet-1/trumpet_1_layout.py
+++ animales/builds/letter-parts/trumpet-1/trumpet_1_layout.py
@@ -3,8 +3,7 @@ import baca
 
 part_identifier = 'TP-1'
 
-def make_systems(measure_numbers, between_systems=20):
-    global_context_padding = 0
+def make_systems(measure_numbers, between_systems, global_context_padding):
     systems = []
     if measure_numbers[0] == 1:
         first_page = True
@@ -26,11 +25,11 @@ def make_systems(measure_numbers, between_systems=20):
 
 breaks = baca.breaks(
     baca.page(
-        *make_systems([1, 11, 21, 31, 41, 51, 61], 24),
+        *make_systems([1, 11, 21, 31, 41, 56, 62], 24, 5),
         number=1,
         ),
     baca.page(
-        *make_systems([76, 88, 99, 111, 121, 131, 141, 151, 161, 171], 21),
+        *make_systems([69, 82, 99, 109, 116, 127, 141, 151, 161, 171], 21, 5),
         number=2,
         ),
     )
@@ -40,3 +39,5 @@ spacing = baca.scorewide_spacing(
     breaks=breaks,
     fallback_duration=(1, 8),
     )
+spacing.override((56, 68), (1, 12))
+spacing.override((109, 118), (1, 12))
