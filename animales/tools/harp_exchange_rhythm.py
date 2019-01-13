e106ebf612794f3d6c2168ff7e1eb07b14948bc1
Trevor Bača <trevor.baca@gmail.com>
2019-01-13 14:27:15 -0600

    Cleaned up typehints.

--- animales/tools/harp_exchange_rhythm.py
+++ animales/tools/harp_exchange_rhythm.py
@@ -1,6 +1,6 @@
 import abjad
 import baca
-from typing import List
+import typing
 from abjadext import rmakers
 
 
@@ -48,7 +48,7 @@ def harp_exchange_rhythm(
     part_to_counts = abjad.OrderedDict()
     for part, indices in part_to_indices.items():
         offset = indices[0]
-        preamble: List[int] = []
+        preamble: typing.List[int] = []
         if offset != 0:
             preamble.append(offset)
         part_to_preamble[part] = preamble
