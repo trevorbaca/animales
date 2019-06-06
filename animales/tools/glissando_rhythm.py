2c775b785418a22231748ede0c850fe2d94135dd
Trevor Bača <trevor.baca@gmail.com>
2019-06-05 22:05:16 -0500

    Cleaned up beam specifiers.

--- animales/tools/glissando_rhythm.py
+++ animales/tools/glissando_rhythm.py
@@ -10,6 +10,9 @@ def glissando_rhythm(rotate: int = 0) -> baca.RhythmCommand:
     return baca.rhythm(
         rewrite_meter=True,
         rhythm_maker=rmakers.TaleaRhythmMaker(
+        beam_specifier=rmakers.BeamSpecifier(
+            beam_each_division=True,
+        ),
             talea=rmakers.Talea(
                 counts=baca.sequence([5, 1, 2, 1]).rotate(n=rotate),
                 denominator=8,
--- animales/tools/pennant_rhythm.py
+++ animales/tools/pennant_rhythm.py
@@ -16,6 +16,9 @@ def pennant_rhythm(
         mask = rmakers.silence(silences)
         dmask = [mask]
     rhythm_maker = rmakers.TaleaRhythmMaker(
+        beam_specifier=rmakers.BeamSpecifier(
+            beam_each_division=True,
+        ),
         division_masks=dmask,
         extra_counts_per_division=extra_counts,
         talea=rmakers.Talea(counts=[1], denominator=16),
