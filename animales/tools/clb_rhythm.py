d55f81d4797f6741667fab79d08044291fe8aecf
Trevor Bača <trevor.baca@gmail.com>
2019-06-05 22:21:37 -0500

    Blacked code.

--- animales/tools/clb_rhythm.py
+++ animales/tools/clb_rhythm.py
@@ -50,9 +50,7 @@ def clb_rhythm(
         extra_counts = [-1]
 
     rhythm_maker = rmakers.TaleaRhythmMaker(
-        beam_specifier=rmakers.BeamSpecifier(
-            beam_each_division=True,
-        ),
+        beam_specifier=rmakers.BeamSpecifier(beam_each_division=True),
         extra_counts_per_division=extra_counts,
         talea=talea,
         tuplet_specifier=rmakers.TupletSpecifier(
