2f3f6bf62107965415689869d84c345cddbeacfa
Trevor Bača <trevor.baca@gmail.com>
2021-07-10 15:26:24 -0400

    Cleaned up fermata measure number metadata.

--- animales/__metadata__.py
+++ animales/__metadata__.py
@@ -4,29 +4,6 @@ metadata = abjad.OrderedDict(
     [
         ("catalog_number", "WD-001"),
         ("composer", "Bača"),
-        (
-            "fermata_measure_numbers",
-            abjad.OrderedDict(
-                [
-                    (
-                        "15",
-                        [94],
-                    ),
-                    (
-                        "18",
-                        [121, 126],
-                    ),
-                    (
-                        "29",
-                        [164],
-                    ),
-                    (
-                        "30",
-                        [174],
-                    ),
-                ]
-            ),
-        ),
         ("forces_tagline", "for orchestra"),
         ("title", "Animales desconocidos"),
         ("year", 2018),
