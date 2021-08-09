787b25ced6f3cbf3778306d9f6e12fa09ea55f8f
Trevor Bača <trevor.baca@gmail.com>
2021-08-09 16:41:50 -0400

    Cleaned up layout files.

--- animales/builds/letter-parts/english-horn/layout.py
+++ animales/builds/letter-parts/english-horn/layout.py
@@ -37,7 +37,6 @@ breaks = baca.breaks(
 )
 
 spacing = baca.spacing(
-    __file__,
     breaks=breaks,
     fallback_duration=(1, 8),
     overrides=(baca.space((127, 174), (1, 16)),),
--- animales/builds/tabloid-score/layout.py
+++ animales/builds/tabloid-score/layout.py
@@ -256,7 +256,6 @@ breaks = baca.breaks(
 )
 
 spacing = baca.spacing(
-    __file__,
     breaks=breaks,
     fallback_duration=(1, 20),
     overrides=(
