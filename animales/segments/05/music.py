6558706029a669d0f311bb8ab5af5ce0779a696e
Trevor Bača <trevor.baca@gmail.com>
2021-09-23 11:52:16 +0200

    Cleaned up definition files.

--- animales/segments/05/music.py
+++ animales/segments/05/music.py
@@ -12,6 +12,7 @@ start = metadata.get("final_measure_number")
 assert start == 23
 
 maker = baca.SegmentMaker(
+    **baca.segments(),
     error_on_not_yet_pitched=True,
     instruments=animales.instruments,
     margin_markups=animales.margin_markups,
