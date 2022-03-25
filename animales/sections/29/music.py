f0e6e1b81c52d46880ae150c981d6f80a9bd9ce3
Trevor Bača <trevor.baca@gmail.com>
2022-03-25 15:07:12 +0100

    Cleaned up tweaks.

--- animales/sections/29/music.py
+++ animales/sections/29/music.py
@@ -79,10 +79,6 @@ commands(
     baca.rehearsal_mark(
         "BB",
         baca.selectors.skip(1 - 1),
-        #        abjad.tweak(
-        #            (0, 6),
-        #            tag=abjad.Tag("+TABLOID_SCORE"),
-        #        ).extra_offset,
         abjad.Tweak(
             r"- \tweak extra-offset #'(0 . 6)", tag=abjad.Tag("+TABLOID_SCORE")
         ),
