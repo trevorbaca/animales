5d3725640cce63ef9123168a9c7e8f321e266100
Trevor Bača <trevor.baca@gmail.com>
2022-04-11 15:47:40 +0200

    Cleaned up selections.

--- animales/sections/10/music.py
+++ animales/sections/10/music.py
@@ -254,7 +254,7 @@ commands(
     "hp1",
     library.parts("Harp"),
     library.harp_exchange_rhythm(2),
-    baca.laissez_vibrer(selector=baca.selectors.ptails()),
+    baca.laissez_vibrer(selector=lambda _: baca.select.ptails(_)),
     baca.pitch("C5"),
     baca.stopped(selector=baca.selectors.pheads()),
 )
@@ -265,7 +265,7 @@ commands(
     "pf1",
     library.parts("Piano"),
     library.harp_exchange_rhythm(3),
-    baca.laissez_vibrer(selector=baca.selectors.ptails()),
+    baca.laissez_vibrer(selector=lambda _: baca.select.ptails(_)),
     baca.pitch("C5"),
     baca.stopped(selector=baca.selectors.pheads()),
 )
@@ -278,7 +278,7 @@ commands(
     "perc3",
     library.parts("Percussion", 3),
     library.harp_exchange_rhythm(0),
-    baca.laissez_vibrer(selector=baca.selectors.ptails()),
+    baca.laissez_vibrer(selector=lambda _: baca.select.ptails(_)),
     baca.pitch("C5"),
 )
 
@@ -333,7 +333,7 @@ commands(
     "cb1",
     library.parts("Contrabass", 1),
     library.harp_exchange_rhythm(1),
-    baca.laissez_vibrer(selector=baca.selectors.ptails()),
+    baca.laissez_vibrer(selector=lambda _: baca.select.ptails(_)),
     baca.note_head_style_harmonic(),
     baca.pitch("Cqf5", do_not_transpose=True),
 )
--- animales/sections/12/music.py
+++ animales/sections/12/music.py
@@ -240,7 +240,7 @@ commands(
     "hp1",
     library.parts("Harp"),
     library.harp_exchange_rhythm(2),
-    baca.laissez_vibrer(selector=baca.selectors.ptails()),
+    baca.laissez_vibrer(selector=lambda _: baca.select.ptails(_)),
     baca.pitch("C5"),
     baca.stopped(selector=baca.selectors.pheads()),
 )
@@ -251,7 +251,7 @@ commands(
     "pf1",
     library.parts("Piano"),
     library.harp_exchange_rhythm(3),
-    baca.laissez_vibrer(selector=baca.selectors.ptails()),
+    baca.laissez_vibrer(selector=lambda _: baca.select.ptails(_)),
     baca.pitch("C5"),
     baca.stopped(selector=baca.selectors.pheads()),
 )
@@ -270,7 +270,7 @@ commands(
     "perc3",
     library.parts("Percussion", 3),
     library.harp_exchange_rhythm(0),
-    baca.laissez_vibrer(selector=baca.selectors.ptails()),
+    baca.laissez_vibrer(selector=lambda _: baca.select.ptails(_)),
     baca.pitch("C5"),
 )
 
@@ -330,7 +330,7 @@ commands(
     "cb1",
     library.parts("Contrabass", 1),
     library.harp_exchange_rhythm(1),
-    baca.laissez_vibrer(selector=baca.selectors.ptails()),
+    baca.laissez_vibrer(selector=lambda _: baca.select.ptails(_)),
     baca.note_head_style_harmonic(),
     baca.pitch("Cqf5", do_not_transpose=True),
 )
