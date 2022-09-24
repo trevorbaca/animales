892006f04882ca6b50fd2d68faddef810a42e919
Trevor Bača <trevor.baca@gmail.com>
2022-09-24 11:42:46 -0400

    Cleaned up music.py files

--- animales/sections/01/music.py
+++ animales/sections/01/music.py
@@ -202,13 +202,13 @@ def make_score():
 
 
 def main():
-    arguments = baca.build.arguments()
+    environment = baca.build.read_environment(__file__, baca.build.argv())
     score, accumulator, voice_name_to_parameter_to_state = make_score()
     metadata, persist, timing = baca.build.section(
         score,
         library.manifests,
         accumulator.time_signatures,
-        baca.path.dictionaries(__file__),
+        environment,
         **baca.interpret.section_defaults(),
         activate=[baca.tags.LOCAL_MEASURE_NUMBER],
         all_music_in_part_containers=True,
@@ -223,7 +223,13 @@ def main():
         include_layout_ly=True,
         includes=["../stylesheet.ily", "header.ily"],
     )
-    baca.build.persist(lilypond_file, metadata, persist, timing, arguments)
+    baca.build.persist(
+        lilypond_file,
+        environment.metadata,
+        environment.persist,
+        timing,
+        environment.arguments,
+    )
 
 
 if __name__ == "__main__":
