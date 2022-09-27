45e40c45284ef8955c07191403f0165f6a3fc698
Trevor Bača <trevor.baca@gmail.com>
2022-09-27 10:25:31 -0400

    Cleaned up metadata.

--- animales/sections/15/music.py
+++ animales/sections/15/music.py
@@ -443,8 +443,8 @@ def main():
     )
     baca.build.persist_lilypond_file(
         lilypond_file,
-        environment.metadata,
-        environment.persist,
+        metadata,
+        persist,
         timing,
         environment.arguments,
     )
